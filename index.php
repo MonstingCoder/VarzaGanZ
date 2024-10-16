368db2c11198e624e913e4321916611bff51dea5
monsco <muhammadvarza1905@gmail.com>
2024-10-16 07:27:52 +0700

    learn-php: v1.01 (isPrime v0.01)

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="description" content="php roadmap">
     <meta name="author" content="monsco">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0.5">
+    <!-- <meta http-equiv="refresh" content="0.5"> -->
     
     <title>Belajar PHP Dasar</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap">
@@ -35,10 +35,44 @@
 <pre>
 <?php
 
-$word = 'hello ';
-$word .= 'world';
+function isPrime($num) {
 
-echo $word;
+    if ($num == 2) return true; // selmatkan angka 2 dari elminasi
+    if ($num < 2) return false; // eliminasi x < 3
+    if (($num % 2) == 0) return false; // eliminasi x genap
+    for ($i = 3; $i * $i <= $num; $i += 2)
+        if (($num % $i) == 0) return false;
+
+    return true;
+
+    // $temp = 0;
+    // for ($i = $num - 2; ($i % $num) == 0; $i -= 2) { // eliminasi x genap
+    //     $temp = $i;
+    // }
+    
+    // if ($temp == $num) return true;
+    // return false;
+    
+    // angka prima : 9, 15, 21, 25, 27, 33, 35, 39, ...
+    // beda         :  +6  +6  +4  +3  +6  +2  +4
+
+}
+
+/*
+    echo "cek angka prima\n\n";
+    for ($i = -20; $i <= 100; $i++) echo "$i : " . isPrime($i) . "\n";
+*/
+
+/*
+    for ($i = -20; $i <= 1_000_000; $i++) {
+        if (isPrime($i)) {
+            echo "$i ADALAH BILANGAN PRIMA!\n";
+            continue;
+        }
+
+        echo "$i bukan bilangan prima :(\n";
+    }
+*/
 
 ?>
 </pre>
