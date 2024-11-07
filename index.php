5d60bbc550ca44dcb3c7abc2ba700cc062e8a3c3
monsco <muhammadvarza1905@gmail.com>
2024-11-07 21:33:11 +0700

    learn-php: v1.02

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php declare(strict_types = 1); ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -6,7 +7,7 @@
     <meta name="description" content="php roadmap">
     <meta name="author" content="monsco">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta http-equiv="refresh" content="0.5"> -->
+    <meta http-equiv="refresh" content="2.5">
     
     <title>Belajar PHP Dasar</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap">
@@ -35,44 +36,61 @@
 <pre>
 <?php
 
-function isPrime($num) {
+$mobil = [
+    'avanza',
+    'xenia',
+    'yaris',
+    'jazz',
+];
+$cars = [
+    'brand' => 'nissan',
+    'model' => 'gtr nismo',
+    'year' => '1905',
+];
 
-    if ($num == 2) return true; // selmatkan angka 2 dari elminasi
-    if ($num < 2) return false; // eliminasi x < 3
-    if (($num % 2) == 0) return false; // eliminasi x genap
-    for ($i = 3; $i * $i <= $num; $i += 2)
-        if (($num % $i) == 0) return false;
-
-    return true;
-
-    // $temp = 0;
-    // for ($i = $num - 2; ($i % $num) == 0; $i -= 2) { // eliminasi x genap
-    //     $temp = $i;
-    // }
-    
-    // if ($temp == $num) return true;
-    // return false;
-    
-    // angka prima : 9, 15, 21, 25, 27, 33, 35, 39, ...
-    // beda         :  +6  +6  +4  +3  +6  +2  +4
-
-}
+define('laptop', [[
+        'brand' => 'macbook pro 16 inch',
+        'chip' => 'm2 max 38 core gpu',
+        'memory' => '96 gb unified memory',
+        'storage' => '8 tb',
+    ], [
+        'brand' => 'asus zenbook',
+        'chip' => 'core ultra 7',
+        'memory' => '32 gb on chip',
+        'storage' => '1 tb',
+    ], [
+        'brand' => 'lenovo slim yoga',
+        'chip' => 'core i7 12600p',
+        'memory' => '16 gb lpddr5',
+        'storage' => '1 tb',
+    ], [
+        'brand' => 'acer helios predator',
+        'chip' => 'core i9 11950hx',
+        'memory' => '64 gb ddr5x',
+        'storage' => '2 tb',
+    ], [
+        'brand' => 'msi bravo',
+        'chip' => 'ryzen 5 5500h',
+        'memory' => '16 gb ddr4',
+        'storage' => '512 gb',
+    ],
+]);
 
 /*
-    echo "cek angka prima\n\n";
-    for ($i = -20; $i <= 100; $i++) echo "$i : " . isPrime($i) . "\n";
-*/
-
-/*
-    for ($i = -20; $i <= 1_000_000; $i++) {
-        if (isPrime($i)) {
-            echo "$i ADALAH BILANGAN PRIMA!\n";
-            continue;
-        }
-
-        echo "$i bukan bilangan prima :(\n";
+    echo "brand\t\tchip\t\t\tstorage";
+    for ($i = 0; $i < count(laptop); $i++) {
+        echo "\n" . laptop[$i]['brand'] . "\t\t" . laptop[$i]['chip'] . "\t\t" . laptop[$i]['storage'];
     }
 */
+// /*
+    foreach(laptop as $arr) {
+        foreach($arr as $key => $val) {
+            echo "$key\t: $val\n";
+        }
+
+        echo "\n";
+    }
+// */
 
 ?>
 </pre>
