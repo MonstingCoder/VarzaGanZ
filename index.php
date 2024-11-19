d9098e7b95fadffd9e811658916d3867dace9d6a
monsco <muhammadvarza1905@gmail.com>
2024-11-19 13:58:44 +0700

    learn-php: v1.2

--- index.php
+++ index.php
@@ -14,77 +14,80 @@
 <div class="title">
     OUTPUT
 </div>
-<div style="font-size: 10px; margin: 2%;">
-    <?php
-    define('super_bike', [
-        'kawaski h2 hyper charge',
-        'ducati panigalle v4 superlegera',
-        'ninja zx25r turbo trickstar',
-        'kawasaki zx4rr',
-        'yamaha nmax 155 abs',
-        'yamaha x-ride 115 twin turbo',
-        'honda adv 165 abs',
-    ]);
-    define('rencana_modif', [
-        'body' => [
-            'speedo meter',
-            'lampu sen',
-            'suspensi depan',
-            'rem belakang',
-        ],
-        'mesin' => [
-            'naikkan rasio kompresi',
-            'upgrade pembakaran',
-            'upgrade knalpot',
-            'servis injeksi',
-            'ganti oli shell merah full sintetik',
-            'kirian' => [
-                'pasang roller 10 gram',
-                'pasang per cvt 2000 rpm',
-                'pasang per sentri 1500 rpm',
-                'papas kipas pulley',
-                'ganti venbelt senyap',
-            ],
-        ],
-        'opsional' => [
-            'pasang turbo',
-            'bore-up tipis 135cc',
-            'lampu depan' => [
-                'lampu dekat hemat daya (always on)',
-                'lampu sorot',
-                'lampu super terang',
-            ],
-        ]
-    ]);
-    ?>
-
-    <form action="<?= $_SERVER['PHP_SELF'];?>" method="get">
-        <label>
-            pilih motor yang ingin dibeli : <br>
-            <select name="motor">
-            <?php foreach (super_bike as $motor):?>
-                <?php if ($motor == super_bike[5]):?>
-                    <option value="<?= htmlspecialchars($motor);?>" selected>
-                        <?= htmlspecialchars($motor);?>
-                    </option>
-                    <?php continue;?>
-                <?php endif;?>
-                <option value="<?= htmlspecialchars($motor);?>">
-                    <?= htmlspecialchars($motor);?>
-                </option>
-            <?php endforeach;?>
-            </select><br>
-            <input onclick="alert('pembelian berhasil')" type="submit" value="beli">
-        </label>
-    </form>
-</div>
 <pre><div class="output"><?php
 
-    $motor_pilihan = htmlspecialchars($_GET['motor']);
-    echo "pilihan\t: $motor_pilihan\n";
-    echo "status\t: akan segera dikirimkan ke alamat anda...\n";
+    $form0 = [
+        'gender' => [
+            'Laki-laki',
+            'Perempuan',
+        ],
+        'kelas' => []
+    ]; for ($i = 1; $i <= 20; $i++) {
+        $form0['kelas'][] = '3IA' . sprintf("%02d", $i);
+    }
+
+    // var_dump($form0);
 
 ?></div></pre>
-
+<div class="others">
+    <div class="form">
+        <h1>
+            MASUK
+        </h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <fieldset>
+                <legend>
+                    Data Siswa :
+                </legend>
+                <label>
+                    Nama <br>
+                    <textarea name="nama"></textarea>
+                </label><br>
+                <datalist id="kelas">
+                    <?php foreach ($form0['kelas'] as $k):?>
+                        <option value="<?= $k?>">
+                            Kelas : <?= $k?>
+                        </option>
+                    <?php endforeach;?>
+                </datalist>
+                <label>
+                    Kelas <br>
+                    <input type="text" list="kelas" name="kelas">
+                </label><br>
+                <label>
+                    Gender <br>
+                    <select name="gender">
+                        <option value="<?= null?>">
+                            Pilih
+                        </option>
+                        <?php foreach($form0['gender'] as $g):?>
+                            <option value="<?= $g?>">
+                                <?= $g?>
+                            </option>
+                        <?php endforeach;?>
+                    </select>
+                </label><br>
+                <label>
+                    Password <br>
+                    <input type="password" name="password">
+                </label><br>
+            </fieldset>
+            <div class="button">
+                <button type="submit">KIRIM</button>
+                <button type="reset">Reset</button>
+            </div>
+        </form>
+    </div>
+    <div class="result">
+        <h1>
+            Data Siswa
+        </h1>
+        <?php foreach ($_POST as $p):?>
+            <p>
+                <?= $p?>
+            </p>
+        <?php endforeach;?> <!-- TUGAS : BUAT OUTPUT DALAM BENTUK TABEL -->
+    </div>
+</div>
 </body>
 </html>
