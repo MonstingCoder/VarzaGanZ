efb93a5570726755aabc1aaff2e457d4404af219
monsco <muhammadvarza1905@gmail.com>
2024-11-19 10:09:30 +0700

    learn-php: v1.07

--- index.php
+++ index.php
@@ -1,76 +1,90 @@
-<?php declare(strict_types = 1); ?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="keywords" content="HTML, CSS, PHP">
-    <meta name="description" content="php roadmap">
-    <meta name="author" content="monsco">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta http-equiv="refresh" content="2.5"> -->
-    
-    <title>Belajar PHP Dasar</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap">
-
-<style>
-
-    body {
-        background-color: black;
-        color: cyan;
-        font-family: 'Fira Code', monospace;
-        font-size: 140%;
-        margin: 20px;
-    }
-
-    p {
-        color: red;
-        font-size: 160%;
-        text-align: center
-    }
-
-</style>
+    <title>
+        php basic
+    </title>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Code&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<form action="<?= $_SERVER['PHP_SELF'];?>" method="post">
-    <label>
-        nama <br>
-        <input name="nama" type="text" value="<?= null;?>">
-    </label><br>
-    <label>
-        umur <br>
-        <input name="umur" type="text" value="<?= null;?>">
-    </label><br>
-    <label>
-        waktu (saat ini) <br>
-        <input name="waktu" type="text" value="<?= null;?>">
-    </label><br><br>
-    <input type="submit" value="kirim">
-    <input type="reset" value="ulangi">
-</form>
-<!-- <?= $_GET['word'];?> add : http://127.0.0.1:8000/?word=hello%20world -->
+<div class="title">
+    OUTPUT
+</div>
+<div style="font-size: 10px; margin: 2%;">
+    <?php
+    define('super_bike', [
+        'kawaski h2 hyper charge',
+        'ducati panigalle v4 superlegera',
+        'ninja zx25r turbo trickstar',
+        'kawasaki zx4rr',
+        'yamaha nmax 155 abs',
+        'yamaha x-ride 115 twin turbo',
+        'honda adv 165 abs',
+    ]);
+    define('rencana_modif', [
+        'body' => [
+            'speedo meter',
+            'lampu sen',
+            'suspensi depan',
+            'rem belakang',
+        ],
+        'mesin' => [
+            'naikkan rasio kompresi',
+            'upgrade pembakaran',
+            'upgrade knalpot',
+            'servis injeksi',
+            'ganti oli shell merah full sintetik',
+            'kirian' => [
+                'pasang roller 10 gram',
+                'pasang per cvt 2000 rpm',
+                'pasang per sentri 1500 rpm',
+                'papas kipas pulley',
+                'ganti venbelt senyap',
+            ],
+        ],
+        'opsional' => [
+            'pasang turbo',
+            'bore-up tipis 135cc',
+            'lampu depan' => [
+                'lampu dekat hemat daya (always on)',
+                'lampu sorot',
+                'lampu super terang',
+            ],
+        ]
+    ]);
+    ?>
 
-<p>Output</p>
-<pre>
-<?php
+    <form action="<?= $_SERVER['PHP_SELF'];?>" method="get">
+        <label>
+            pilih motor yang ingin dibeli : <br>
+            <select name="motor">
+            <?php foreach (super_bike as $motor):?>
+                <?php if ($motor == super_bike[5]):?>
+                    <option value="<?= htmlspecialchars($motor);?>" selected>
+                        <?= htmlspecialchars($motor);?>
+                    </option>
+                    <?php continue;?>
+                <?php endif;?>
+                <option value="<?= htmlspecialchars($motor);?>">
+                    <?= htmlspecialchars($motor);?>
+                </option>
+            <?php endforeach;?>
+            </select><br>
+            <input onclick="alert('pembelian berhasil')" type="submit" value="beli">
+        </label>
+    </form>
+</div>
+<pre><div class="output"><?php
 
-$nama = htmlspecialchars($_POST['nama']);
-$umur = (int) htmlspecialchars($_POST['umur']);
-$waktu = htmlspecialchars($_POST['waktu']);
+    $motor_pilihan = htmlspecialchars($_GET['motor']);
+    echo "pilihan\t: $motor_pilihan\n";
+    echo "status\t: akan segera dikirimkan ke alamat anda...\n";
 
-function sapa(
-    $nama = 'admin',
-    $waktu = 'datang'
-) {
-    return htmlspecialchars("hallo $nama, selamat $waktu\n");
-}
-
-echo "default\t: ", sapa();
-echo "filled\t: ", sapa($nama, $waktu);
-
-?>
-</pre>
-<input type="color" name="" id="">
+?></div></pre>
 
 </body>
 </html>
